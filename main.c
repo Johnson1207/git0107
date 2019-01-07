9f88d7e9a9fbe100c1a0fff75b870fa973e85d9e
Tsai <johnsan11111@gmail.com>
2019-01-07 17:13:02 +0800

    merge1

--- main.c
+++ main.c
@@ -2,18 +2,15 @@
 #include <stdlib.h>
 
 int main()
-<<<<<<< HEAD
 {
+    int a;
+
     printf("Hello world!\n\n");
-    printf(:"Group3\n\n");
+    printf("Group3\n\n");
     printf("A1065145蔡孟軒\n");
 
-=======
-{   int a;
-    printf("Hello world!\n");
     printf("alice\n");
     printf("Enter your number:");
     scanf("%d",&a);
->>>>>>> alice
     return 0;
 }
